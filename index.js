9af01b1083d0f57d5197b3097b5c4fc4adfc3357
Kopal Gupta <gkopal7@outlook.com>
2017-03-15 23:14:10 +0530

    using coordinates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-	var div = document.getElementById('one');
+	// var div = document.getElementById('one');
 	// var x, y;
 
  //    var drag = false;
@@ -19,26 +19,55 @@
  //        div.style.top = y + "px";
  //    };
 
-    var drag = false, x, y, posx, posy, first = true;
-    div.onmousedown = function() {
-        drag = true;
-    };
-    div.onmouseup = function() {
-        drag = false;
-        first = true;
-    };
-    div.onmousemove = function(e) {
-        if (drag) {
-            if (first) {
-                x = e.offsetX;
-                y = e.offsetY;
-                first = false;
-            }
-            posx = e.clientX - x;
-            posy = e.clientY - y;
-            posx = e.clientX;
-            posy = e.clientY;
-            this.style.left = posx + 'px';
-            this.style.top = posy + 'px';
-        }
-    }; 
+// var drag = false, x, y, posx, posy, first = true;
+// div.onmousedown = function() {
+//     drag = true;
+// };
+// div.onmouseup = function() {
+//     drag = false;
+//     first = true;
+// };
+// div.onmousemove = function(e) {
+//     if (drag) {
+//         if (first) {
+//             x = e.offsetX;
+//             y = e.offsetY;
+//             first = false;
+//         }
+//         posx = e.clientX - x;
+//         posy = e.clientY - y;
+//         posx = e.clientX;
+//         posy = e.clientY;
+//         this.style.left = posx + 'px';
+//         this.style.top = posy + 'px';
+//     }
+// }; 
+
+var el = document.getElementById('one');
+    var eltop = 0, elleft = 0;
+    var mouseX = 0, mouseY = 0;
+    document.addEventListener('mousedown','down');
+    document.addEventListener('mouseup','up');
+
+    function down(e) {
+     if (e.target === el) {
+         mouseX = e.clientX;
+         mouseY = e.clientY;
+
+         eltop = Number(el.style.top.val.replace('px', ''));
+         elleft = Number(el.style.left.val.replace('px', ''));
+
+         document.addEventListener('nmousemove','move');
+     }
+    }
+
+    function move(e) {
+     var delX = e.clientX - mouseX;
+     var delY = e.clientY - mouseY;
+     el.style.top = (eltop + delY) + "px";
+     el.style.left = (elleft + delX) + "px";
+    }
+
+    function up(e) {
+     document.removeEventListener('mousemove','move');
+    }
