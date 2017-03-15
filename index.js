2c27de0843653ca31a9bdf882526b35c3f28729a
Kopal Gupta <gkopal7@outlook.com>
2017-03-15 22:48:13 +0530

    minor additions

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 	var div = document.getElementById('one');
-	var x, y;
+	// var x, y;
 
-    var drag = false;
+ //    var drag = false;
+ //    div.onmousedown = function() {
+ //        drag = true;
+ //    };
+ //    div.onmouseup = function() {
+ //        drag = false;
+ //    };
+ //    div.onmousemove = function(event) {
+ //        if (drag) {
+ //            x = event.clientX;
+ //            y = event.clientY;
+ //         	// div.style.left = x + "px";
+ //        	// div.style.top = y + "px";
+ //        }
+ //        div.style.left = x + "px";
+ //        div.style.top = y + "px";
+ //    };
+
+    var drag = false, x, y, posx, posy, first = true;
     div.onmousedown = function() {
         drag = true;
     };
     div.onmouseup = function() {
         drag = false;
+        first = true;
     };
-    div.onmousemove = function(event) {
+    div.onmousemove = function(e) {
         if (drag) {
-            x = event.clientX;
-            y = event.clientY;
-         	// div.style.left = x + "px";
-        	// div.style.top = y + "px";
+            if (first) {
+                x = e.offsetX;
+                y = e.offsetY;
+                first = false;
+            }
+            posx = e.clientX - x;
+            posy = e.clientY - y;
+            posx = e.clientX;
+            posy = e.clientY;
+            this.style.left = posx + 'px';
+            this.style.top = posy + 'px';
         }
-        div.style.left = x + "px";
-        div.style.top = y + "px";
-    };
-
-    
+    }; 
